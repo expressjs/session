211e7e98e30299930ef7209247682a849a5ee2be
rocco <rocco@hackerstribe.com>
2017-07-19 13:06:35 +0200

    code clean

--- session/memory.js
+++ session/memory.js
@@ -15,22 +15,10 @@
 
 var Store = require('./store')
 var util = require('util')
-
-/**
- * Shim setImmediate for node.js < 0.10
- * @private
- */
-
-/* istanbul ignore next */
-// causing fail??
-var defer = typeof setImmediate === 'function'
-  ? setImmediate
-  : function(fn){ process.nextTick(fn.bind.apply(fn, arguments)) }
+var MemoryStore = require('memorystore')
 
 /**
  * Module exports.
  */
-
-var MemoryStore = require('memorystore')
 
 module.exports = new MemoryStore({Store: Store})
