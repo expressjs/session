104725ebcb1c8ef9a242ea18577d1f34885974a0
Douglas Christopher Wilson <doug@somethingdoug.com>
2018-04-01 14:33:45 -0400

    tests: add missing tests for "name" option

--- test/session.js
+++ test/session.js
@@ -880,6 +880,22 @@ describe('session()', function(){
     })
   })
 
+  describe('name option', function () {
+    it('should default to "connect.sid"', function (done) {
+      request(createServer())
+      .get('/')
+      .expect(shouldSetCookie('connect.sid'))
+      .expect(200, done)
+    })
+
+    it('should set the cookie name', function (done) {
+      request(createServer({ name: 'session_id' }))
+      .get('/')
+      .expect(shouldSetCookie('session_id'))
+      .expect(200, done)
+    })
+  })
+
   describe('rolling option', function(){
     it('should default to false', function(done){
       var server = createServer(null, function (req, res) {
