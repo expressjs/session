50c11b86bf9bda436754837b6acbf67932534201
HoldYourWaffle <ravivanrooijen@live.nl>
2019-09-29 21:41:24 +0200

    Use instance methods instead of lambda properties

--- index.d.ts
+++ index.d.ts
@@ -77,27 +77,27 @@ interface Session extends SessionData {
 
 
 export abstract class Store extends node.EventEmitter {
-  regenerate: (req: express.Request, callback: (err?: any) => any) => void;
-  load: (sid: string, callback: (err: any, session?: SessionData) => any) => void;
-  createSession: (req: express.Request, session: SessionData) => void;
+  regenerate(req: express.Request, callback: (err?: any) => any): void
+  load(sid: string, callback: (err: any, session?: SessionData) => any): void
+  createSession(req: express.Request, session: SessionData): void
 
-  abstract get: (sid: string, callback: (err: any, session?: SessionData | null) => void) => void;
-  abstract set: (sid: string, session: SessionData, callback?: (err?: any) => void) => void;
-  abstract destroy: (sid: string, callback?: (err?: any) => void) => void;
+  abstract get(sid: string, callback: (err: any, session?: SessionData | null) => void): void
+  abstract set(sid: string, session: SessionData, callback?: (err?: any) => void): void
+  abstract destroy(sid: string, callback?: (err?: any) => void): void
 
-  all?: (callback: (err: any, obj?: { [sid: string]: SessionData; } | null) => void) => void;
-  length?: (callback: (err: any, length: number) => void) => void;
-  clear?: (callback?: (err?: any) => void) => void;
-  touch?: (sid: string, session: SessionData, callback?: () => void) => void;
+  all?(callback: (err: any, obj?: { [sid: string]: SessionData; } | null) => void): void
+  length?(callback: (err: any, length: number) => void): void
+  clear?(callback?: (err?: any) => void): void
+  touch?(sid: string, session: SessionData, callback?: () => void): void
 }
 
 export class MemoryStore extends Store {
-  get: (sid: string, callback: (err: any, session?: SessionData | null) => void) => void;
-  set: (sid: string, session: SessionData, callback?: (err?: any) => void) => void;
-  destroy: (sid: string, callback?: (err?: any) => void) => void;
+  get(sid: string, callback: (err: any, session?: SessionData | null) => void): void
+  set(sid: string, session: SessionData, callback?: (err?: any) => void): void
+  destroy(sid: string, callback?: (err?: any) => void): void
 
-  all: (callback: (err: any, obj?: { [sid: string]: SessionData; } | null) => void) => void;
-  length: (callback: (err: any, length: number) => void) => void;
-  clear: (callback?: (err?: any) => void) => void;
-  touch: (sid: string, session: SessionData, callback?: () => void) => void;
+  all(callback: (err: any, obj?: { [sid: string]: SessionData; } | null) => void): void
+  length(callback: (err: any, length: number) => void): void
+  clear(callback?: (err?: any) => void): void
+  touch(sid: string, session: SessionData, callback?: () => void): void
 }
