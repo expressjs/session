0a27782df6912b688f597ea1dffd818f2cc02437
es3 <jason.scz@gmail.com>
2019-10-11 04:01:55 -0400

    Add Promise return to req.session.save()
    Now we can use req.session.save() function as promise example
    
    await req.session.save()
        .then(()=>{
            console.log('saved')
        })
        .catch(()=>{
            console.log('error')
        })

--- index.js
+++ index.js
@@ -393,6 +393,7 @@ function session(options) {
         debug('saving %s', this.id);
         savedHash = hash(this);
         _save.apply(this, arguments);
+        return this.req.sessionStore.setPromise
       }
 
       Object.defineProperty(sess, 'reload', {
