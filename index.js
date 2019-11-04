6c7d57068a0bbfb824ee6f1e92f3f91bea7b4836
Gireesh Punathil <gpunathi@in.ibm.com>
2019-11-04 14:45:32 +0530

    src: more debug statements
    Add debug statement (enabled only in debug mode)
    at vital session life cycle control flow points
    
    Refs: https://github.com/expressjs/session/issues/633#issuecomment-471110093

--- index.js
+++ index.js
@@ -168,6 +168,7 @@ function session(options) {
   return function session(req, res, next) {
     // self-awareness
     if (req.session) {
+      debug('re-using existing session')
       next()
       return
     }
@@ -295,13 +296,13 @@ function session(options) {
 
       if (shouldDestroy(req)) {
         // destroy session
-        debug('destroying');
+        debug('destroying session store');
         store.destroy(req.sessionID, function ondestroy(err) {
           if (err) {
             setImmediate(next, err);
           }
 
-          debug('destroyed');
+          debug('session store destroyed');
           writeend();
         });
 
@@ -326,6 +327,7 @@ function session(options) {
             setImmediate(next, err);
           }
 
+          debug('session saved');
           writeend();
         });
 
@@ -350,6 +352,7 @@ function session(options) {
 
     // generate the session
     function generate() {
+      debug('session generate');
       store.generate(req);
       originalId = req.sessionID;
       originalHash = hash(req.session);
