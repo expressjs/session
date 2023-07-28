12af59f74c065440a78f29e35c189eba8f7ce408
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-07-28 20:40:09 +0200

    Make eslint work again.

--- session/memory.js
+++ session/memory.js
@@ -21,79 +21,78 @@ const Store = require('./store');
 
 /* istanbul ignore next */
 const defer = typeof setImmediate === 'function'
-    ? setImmediate
-    : function(fn){ process.nextTick(fn.bind.apply(fn, arguments)) }
+  ? setImmediate
+  : function(fn){ process.nextTick(fn.bind.apply(fn, arguments)) }
 
 /**
  * Module exports.
  */
 
 
-    /**
+/**
      * A session store in memory.
      * @public
      */
-    class MemoryStore extends Store {
-      sessions;
-      constructor() {
-        super()
-        this.sessions = Object.create(null)
-      }
+class MemoryStore extends Store {
+  constructor() {
+    super()
+    this.sessions = Object.create(null)
+  }
 
-      /**
+  /**
        * Get all active sessions.
        *
        * @param {function} callback
        * @public
        */
-      all = (callback) =>{
-        const sessionIds = Object.keys(this.sessions)
-        const sessions = Object.create(null)
+  all(callback){
+    const sessionIds = Object.keys(this.sessions)
+    const sessions = Object.create(null)
 
-        for (let i = 0; i < sessionIds.length; i++) {
-          const sessionId = sessionIds[i]
-          const session = this.getSession.call(this, sessionId)
+    for (let i = 0; i < sessionIds.length; i++) {
+      const sessionId = sessionIds[i]
+      const session = this.getSession.call(this, sessionId)
 
-          if (session) {
-            sessions[sessionId] = session;
-          }
-        }
-
-        callback && defer(callback, null, sessions)
+      if (session) {
+        sessions[sessionId] = session;
       }
-      /**
+    }
+
+    callback && defer(callback, null, sessions)
+  }
+  /**
        * Clear all sessions.
        *
        * @param {function} callback
        * @public
        */
-      clear = (callback) =>{
-        this.sessions = Object.create(null)
-        callback && defer(callback)
-      }
-      /**
+  clear(callback){
+    this.sessions = Object.create(null)
+    callback && defer(callback)
+  }
+  /**
        * Destroy the session associated with the given session ID.
        *
        * @param {string} sessionId
        * @param callback
        * @public
        */
-      destroy = (sessionId, callback) =>{
-        delete this.sessions[sessionId]
-        callback && defer(callback)
-      }
-      /**
+  destroy(sessionId, callback){
+    delete this.sessions[sessionId]
+    callback && defer(callback)
+  }
+  /**
        * Fetch session by the given session ID.
        *
        * @param {string} sessionId
        * @param {function} callback
        * @public
        */
-      get = (sessionId, callback) =>{
-        defer(callback, null, this.getSession.call(this, sessionId))
-      }
+  get(sessionId, callback){
+    defer(callback, null, this.getSession.call(this, sessionId))
+  }
 
-      /**
+  /**
        * Commit the given session associated with the given sessionId to the store.
        *
        * @param {string} sessionId
@@ -101,27 +100,27 @@ const defer = typeof setImmediate === 'function'
        * @param {function} callback
        * @public
        */
-      set = (sessionId, session, callback)=> {
-        session.req&&delete session.req.value
-        this.sessions[sessionId] = JSON.stringify(session)
-        callback && defer(callback)
-      }
+  set(sessionId, session, callback) {
+    session.req&&delete session.req.value
+    this.sessions[sessionId] = JSON.stringify(session)
+    callback && defer(callback)
+  }
 
-      /**
+  /**
        * Get number of active sessions.
        *
        * @param {function} callback
        * @public
        */
 
-      length = (callback) =>{
-        this.all(function (err, sessions) {
-          if (err) return callback(err)
-          callback(null, Object.keys(sessions).length)
-        })
-      }
+  length(callback){
+    this.all(function (err, sessions) {
+      if (err) return callback(err)
+      callback(null, Object.keys(sessions).length)
+    })
+  }
 
-      /**
+  /**
        * Touch the given session object associated with the given session ID.
        *
        * @param {string} sessionId
@@ -129,57 +128,57 @@ const defer = typeof setImmediate === 'function'
        * @param {function} callback
        * @public
        */
-      touch = (sessionId, session, callback) =>{
-        const currentSession = this.getSession.call(this, sessionId)
+  touch(sessionId, session, callback){
+    const currentSession = this.getSession.call(this, sessionId)
 
-        if (currentSession) {
-          // update expiration
-          currentSession.cookie = session.cookie
-          this.sessions[sessionId] = JSON.stringify(currentSession)
-        }
+    if (currentSession) {
+      // update expiration
+      currentSession.cookie = session.cookie
+      this.sessions[sessionId] = JSON.stringify(currentSession)
+    }
 
-        callback && defer(callback)
-      }
+    callback && defer(callback)
+  }
 
-      /**
+  /**
        * Get session from the store.
        * @private
        */
-      getSession = (sessionId) =>{
-        let sess
-        let unwrapped_id
+  getSession(sessionId){
+    let sess
+    let unwrapped_id
 
-        if (typeof sessionId === 'string') {
-          unwrapped_id = sessionId
-        } else {
-          // @ts-ignore
-          unwrapped_id = sessionId.value
-        }
+    if (typeof sessionId === 'string') {
+      unwrapped_id = sessionId
+    } else {
+      // @ts-ignore
+      unwrapped_id = sessionId.value
+    }
 
-        sess = this.sessions[unwrapped_id]
+    sess = this.sessions[unwrapped_id]
 
-        if (!sess) {
-          return
-        }
+    if (!sess) {
+      return
+    }
 
-        // parse
-        sess = JSON.parse(sess)
+    // parse
+    sess = JSON.parse(sess)
 
-        if (sess.cookie) {
-          const expires = typeof sess.cookie.expires === 'string'
-              ? new Date(sess.cookie.expires)
-              : sess.cookie.expires
+    if (sess.cookie) {
+      const expires = typeof sess.cookie.expires === 'string'
+        ? new Date(sess.cookie.expires)
+        : sess.cookie.expires
 
-          // destroy expired session
-          if (expires && expires <= Date.now()) {
-            delete this.sessions[unwrapped_id]
-            return
-          }
-        }
-
-        return sess
+      // destroy expired session
+      if (expires && expires <= Date.now()) {
+        delete this.sessions[unwrapped_id]
+        return
       }
     }
+
+    return sess
+  }
+}
 module.exports = MemoryStore;
 
 
